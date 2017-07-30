6c187c384867f32b932a4d9a9fab3b0a07ca1307
Dillon Raphael <dillonraphael@gmail.com>
2017-07-30 14:51:34 -0400

    Contact form sends email with nodemailer

--- components/ContactForm.js
+++ components/ContactForm.js
@@ -1,23 +1,81 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
+import axios from 'axios'
+
+const instance = axios.create({baseURL: 'http://localhost:3000/api/v1'})
+
 
 class ContactForm extends Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			fullname: '',
+			email: '',
+			msg: ''
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this)
+		this.handleChange = this.handleChange.bind(this)
+	} 
+
+	componentDidMount(){
+		this.setState({
+			fullname: '',
+			email: '',
+			msg: ''
+		})
+	}
+
+	submitForm(data) {
+		instance.post('sendMail', data).then((response) => {
+			console.log(response)
+		}).catch((error) => {
+			console.log(error)
+		})
+	}
+
+  handleChange(e) {
+    const value = e.target.value
+    this.setState({
+      [e.target.name]: value
+    })
+  }
+
+	handleSubmit(e) {
+		e.preventDefault()
+		this.submitForm(this.state)
+		e.target.fullname.value = ''
+		e.target.email.value = ''
+		e.target.msg.value = ''
+
 	}
 
 	render() {
 		return (
 			<div className="ContactForm">
-				<form>
+				<form onSubmit={this.handleSubmit}>
 					<div className="field">
-						<input className="input" type="text" placeholder="fullname" name="fullname" />
+						<input 
+						className="input" 
+						type="text" 
+						placeholder="fullname" 
+						name="fullname"
+						onChange={this.handleChange} />
 					</div>
 					<div className="field">
-						<input className="input" type="email" placeholder="email" name="email" />
+						<input 
+						className="input" 
+						type="email" 
+						placeholder="email" 
+						name="email"
+						onChange={this.handleChange} />
 					</div>
 					<div className="field">
-						<textarea className="textarea" placeholder="msg...." name="message" />
+						<textarea 
+						className="textarea" 
+						placeholder="msg...." 
+						name="msg"
+						onChange={this.handleChange} />
 					</div>
 					<div>
 						<input className="button" type="submit" />
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ import passport from 'passport'
 
 import pageController from '../controllers/pageController'
 import userController from '../controllers/userController'
+import mailController from '../controllers/mailController'
 
 const router = express.Router()
 
@@ -18,4 +19,5 @@ router.delete(`${apiPrefix}/deletePage/:id`, pageController.deletePage);
 router.post(`${apiPrefix}/register`, userController.register);
 router.post(`${apiPrefix}/login`, userController.signin);
 
+router.post(`${apiPrefix}/sendMail`, mailController.contactForm)
 module.exports = router;
